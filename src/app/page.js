34749adf10976e1b4f7d7887abc3ee7b1e0cb431
alx-calcio <alexandre.calciogaudino@gmail.com>
2025-06-20 14:05:09 +0200

    Deleted hoover effect

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <FuzzyText fontSize={100} baseIntensity={0.3} enableHover={true} hoverIntensity={0.9}>Salut</FuzzyText>
+      <FuzzyText fontSize={100} baseIntensity={0.3}>Salut</FuzzyText>
       <br/>
-      <FuzzyText fontSize={30} baseIntensity={0.3} enableHover={true} hoverIntensity={0.9}>Comment ça va ?</FuzzyText>
+      <FuzzyText fontSize={30} baseIntensity={0.1}>Comment ça va ?</FuzzyText>
     </div>
   );
 }
