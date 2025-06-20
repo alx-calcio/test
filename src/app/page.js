a2e26fcf3b9126b56f4aa84b3932aeec2667976d
alx-calcio <alexandre.calciogaudino@gmail.com>
2025-06-20 14:02:38 +0200

    Re-edited fontsize

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <FuzzyText fontSize={150} baseIntensity={0.3} enableHover={true} hoverIntensity={0.9}>Salut</FuzzyText>
-      <FuzzyText fontSize={70} baseIntensity={0.3} enableHover={true} hoverIntensity={0.9}>Comment ça va ?</FuzzyText>
+      <FuzzyText fontSize={120} baseIntensity={0.3} enableHover={true} hoverIntensity={0.9}>Salut</FuzzyText>
+      <FuzzyText fontSize={50} baseIntensity={0.3} enableHover={true} hoverIntensity={0.9}>Comment ça va ?</FuzzyText>
     </div>
   );
 }
